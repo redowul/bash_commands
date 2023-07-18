d82bf789a41bda6190fd11dc8809d7b8e56e34c6
Connor <connorcolabella@gmail.com>
2023-07-18 11:21:01 -0400

    Gave deckfinder.sh sftp capabilities

--- deckfinder.sh
+++ deckfinder.sh
@@ -12,8 +12,13 @@ function scan {
 }
 
 function ssh_connect {
-    echo "Attempting to connect to $target_name at $steamdeck_ip_address on port $target_port..."
-    ssh -o ConnectTimeout=$connect_timeout "$username"@"$steamdeck_ip_address" -p "$target_port"
+    local connection_type=${1:-ssh}  # default to ssh if no argument passed
+    echo "Attempting to connect to $target_name at $steamdeck_ip_address on port $target_port using $connection_type..."
+    if [ "$connection_type" == "sftp" ]; then
+        sftp -oPort=$target_port $username@$steamdeck_ip_address
+    else
+        ssh -o ConnectTimeout=$connect_timeout $username@$steamdeck_ip_address -p $target_port
+    fi
 }
 
 # Check if IP address variable is already set
@@ -21,12 +26,15 @@ if [ -z "$steamdeck_ip_address" ]; then
     scan
 fi
 
-# Attempt to connect via SSH
-ssh_connect
+# Get the connection type from the first command line argument (default to ssh if not specified)
+connection_type=${1:-ssh}
 
-# If the SSH connection fails or times out, perform the scan again and retry the connection
+# Attempt to connect
+ssh_connect $connection_type
+
+# If the connection fails or times out, perform the scan again and retry the connection
 if [ $? -ne 0 ]; then
-    echo "SSH connection failed or timed out, rescanning network..."
+    echo "$connection_type connection failed or timed out, rescanning network..."
     scan
-    ssh_connect
+    ssh_connect $connection_type
 fi
